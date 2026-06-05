6cf675e750932d7b80aa5a461c00226d0da39fc3
Joey Hoechsmann <joey@hushhf.com>
2026-06-05 13:38:34 -0600

    Fix wildcard route for Express compatibility

--- server/server.js
+++ server/server.js
@@ -153,7 +153,7 @@ app.put('/api/settings/:key', async (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')))
-  app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'))
   })
 }
